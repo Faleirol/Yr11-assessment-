f03e66320596daa1853ac0bcfafa24e917cb83a0
faleirol <faleirol@stu.otc.school.nz>
2021-09-10 13:01:16 +1200

    Checking if commit works for 2nd function

---
+++ Main character function.py
@@ -0,0 +1,6 @@
+Main_character_list = ["Criminal", "Hero", "Villain", "Teenager", "Mythical character", "Alien", "Human", "Animal"]
+Main_character_fun = input("Pick a main character")
+if Main_character_fun == Main_character_list:
+    print("data stored")
+else:
+    print("pick one in the list")
