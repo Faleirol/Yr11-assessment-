9a15722e54a79d8c23c9563e01b4304d97862462
faleirol <faleirol@stu.otc.school.nz>
2021-09-10 13:20:56 +1200

    Forgot to add a print statement for main_character_list

--- Main character function.py
+++ Main character function.py
@@ -1,4 +1,5 @@
 Main_character_list = ["Criminal", "Hero", "Villain", "Teenager", "Mythical character", "Alien", "Human", "Animal"]
+print(Main_character_list)
 Main_character_fun = input("Pick a main character")
 if Main_character_fun == Main_character_list:
     print("data stored")
