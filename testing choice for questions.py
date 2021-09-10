51cc21d355ce9992454dc473aba939248fbca540
faleirol <faleirol@stu.otc.school.nz>
2021-09-10 13:27:22 +1200

    the if statement keeps printing else "pick the 2 options". need to find out why.

---
+++ testing choice for questions.py
@@ -0,0 +1,9 @@
+print("Welcome to Story Generator!")
+Choice_for_questions = "Would you like to run all the questions or just one?"
+print(Choice_for_questions)
+if Choice_for_questions == "All":
+    print("All function")
+elif Choice_for_questions == "One":
+    print("Single function")
+else:
+    print("Pick the 2 options")
