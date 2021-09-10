278c9793e8ab917c204a9983e53fe8cbcb4e96ca
faleirol <faleirol@stu.otc.school.nz>
2021-09-10 12:59:41 +1200

    Checking if commit works for main

---
+++ generator_program.py
@@ -0,0 +1,10 @@
+
+print("Welcome to Story Generator!")
+Choice_for_questions = "Would you like to run all the questions or just one?"
+print(Choice_for_questions)
+# All the list information
+Genre_list = ["Horror", "Sci-fi", "Comedy", "Thriller", "Drama", "Fantasy", "Action", "Crime"]
+Main_character_list = ["Criminal", "Hero", "Villain", "Teenager", "Mythical character", "Alien", "Human", "Animal"]
+Num_character_list = ["1 character", "A duo", "3 friends", "A group (4+)"]
+Ending_list = ["Happy ending", "Sad ending", "Cliffhanger ending", "Abrupt ending", "Plot twist ending"]
+Choice_list = ["Genre", "Main character", "Amount of main characters", "Type of ending"]
