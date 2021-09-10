5f69d52946e108dea52358a82758aeb68e90f05f
faleirol <faleirol@stu.otc.school.nz>
2021-09-10 12:58:17 +1200

    Checking if commit works

---
+++ Genre Function.py
@@ -0,0 +1,6 @@
+Genre_list = ["Horror", "Sci-fi", "Comedy", "Thriller", "Drama", "Fantasy", "Action", "Crime"]
+Genre_fun = input("Pick a genre")
+if Genre_fun == Genre_list:
+    print("your data stored")
+else:
+    print("pick a genre in the list")
