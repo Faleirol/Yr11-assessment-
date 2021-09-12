8d29ca0e0ac089a82668271708a7994360a8f16f
faleirol <faleirol@stu.otc.school.nz>
2021-09-13 10:54:55 +1200

    i made a list for the options of answers the user could type. and fixed the problem with the if statement not working. It wouldnt process the list with the data of the user, but by puttin in instead of == it worked.

--- testing choice for questions.py
+++ testing choice for questions.py
@@ -1,9 +1,11 @@
 print("Welcome to Story Generator!")
-Choice_for_questions = "Would you like to run all the questions or just one?"
+All_answer = ["All", "all", "ALL", "All questions", "all questions", "ALL QUESTIONS"]
+Single_answer = ["Single", "single", "SINGLE", "One", "one", "ONE"]
+Choice_for_questions = input("Would you like to run all the questions or just one?")
 print(Choice_for_questions)
-if Choice_for_questions == "All":
+if Choice_for_questions in All_answer:
     print("All function")
-elif Choice_for_questions == "One":
+elif Choice_for_questions in Single_answer:
     print("Single function")
 else:
     print("Pick the 2 options")
