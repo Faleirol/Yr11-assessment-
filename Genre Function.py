926b09af59e90b7bf79c14f86c87b5a893dd94e3
faleirol <faleirol@stu.otc.school.nz>
2021-09-13 10:55:33 +1200

    i made a list for the options of answers the user could type. and fixed the problem with the if statement not working. It wouldnt process the list with the data of the user, but by puttin in instead of == it worked.

--- Genre Function.py
+++ Genre Function.py
@@ -1,7 +1,8 @@
+Genre_answer = ["Horror", "horror", "HORROR", "Sci-fi", "sci-fi", "SCI-FI", "sci fi", "Sci fi", "SCI FI", "Comedy", "comedy", "COMEDY", "Thriller", "thriller", "THRILLER", "Drama", "drama", "DRAMA", "Fantasy", "fantasy", "FANTASY", "Action", "action", "ACTION", "Crime", "crime", "CRIME"]
 Genre_list = ["Horror", "Sci-fi", "Comedy", "Thriller", "Drama", "Fantasy", "Action", "Crime"]
 print(Genre_list)
 Genre_fun = input("Pick a genre")
-if Genre_fun == Genre_list:
+if Genre_fun in Genre_answer:
     print("your data stored")
 else:
     print("pick a genre in the list")
