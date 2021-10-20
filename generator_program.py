6a30f466713ed1b72685294a3050c93c952ff5b3
faleirol <faleirol@stu.otc.school.nz>
2021-10-21 09:36:34 +1300

    Version 2 update
    Re-wrote the single question function to return only a single answer to main.
    Simplified the all questions function - deleted redundant duplicated code.

--- generator_program.py
+++ generator_program.py
@@ -1,57 +1,5 @@
 # version 2 branch
 # all the questions function
-def all_question():
-    genre_answer = ["Horror", "horror", "HORROR", "Sci-fi", "sci-fi", "SCI-FI", "sci fi", "Sci fi", "SCI FI", "Comedy",
-                    "comedy", "COMEDY", "Thriller", "thriller", "THRILLER", "Drama", "drama", "DRAMA", "Fantasy",
-                    "fantasy", "FANTASY", "Action", "action", "ACTION", "Crime", "crime", "CRIME"]
-    genre_list = ["Horror", "Sci-fi", "Comedy", "Thriller", "Drama", "Fantasy", "Action", "Crime"]
-    print(genre_list)
-    User_genre = input("Pick a genre")
-    while User_genre not in genre_answer:
-        User_genre = input("Pick a genre")
-
-    print("Data stored")
-
-    main_character_list = ["Criminal", "Hero", "Villain", "Teenager", "Mythical character", "Alien", "Human", "Animal"]
-    print(main_character_list)
-    main_answer = ["Criminal", "criminal", "CRIMINAL", "Hero", "hero", "HERO", "Villain", "villain", "VILLAIN",
-                   "Teenager", "teenager", "TEENAGER", "Mythical character", "mythical character", "MYTHICAL CHARACTER",
-                   "Alien", "alien", "ALIEN", "Human", "human", "HUMAN", "Animal", "animal", "ANIMAL"]
-    User_main = input("Pick a main character")
-    while User_main not in main_answer:
-        User_main = input("Pick a main character")
-
-    print("Data stored")
-
-    num_character_list = ["1 character", "A duo", "3 character", "A group (4+)"]
-    print(num_character_list)
-    num_answer = ["1 character", "one character", "One Character", "One character", " ONE CHARACTER", "1 CHARACTER",
-                  "A duo", "a duo", "A DUO", "a Duo", "duo", "Duo", "DUO", "3 character", "3 Character", "3 CHARACTER",
-                  "Three character", "three character", "Three Character", "three Character", "THREE CHARACTER",
-                  "A group", "a group", "A Group", "A GROUP"]
-    User_num = input("Pick an amount of main character(s)")
-    while User_num not in num_answer:
-        User_num = input("Pick an amount of main character(s)")
-
-    print("Data stored")
-
-    ending_list = ["Happy ending", "Sad ending", "Cliffhanger ending", "Abrupt ending", "Plot twist ending"]
-    print(ending_list)
-    end_answer = ["Happy ending", "happy ending", "Happy Ending", "HAPPY ENDING", "Happy", "happy", "HAPPY",
-                  "Sad ending", "sad ending", "Sad Ending", "SAD ENDING", "Sad", "sad", "SAD", "Cliffhanger ending",
-                  "cliffhanger ending", "Cliffhanger Ending", "CLIFFHANGER ENDING", "Cliffhanger", "cliffhanger",
-                  "CLIFFHANGER", "Abrupt ending", "abrupt ending", "Abrupt Ending", "ABRUPT ENDING", "Abrupt", "abrupt",
-                  "ABRUPT", "Plot twist ending", "Plot Twist Ending", "plot twist ending", "PLOT TWIST ENDING",
-                  "Plot twist", "plot twist", "Plot Twist", "PLOT TWIST"]
-    User_end = input("Pick a type of ending")
-    while User_end not in end_answer:
-        User_end = input("Pick a type of ending")
-
-    print("Data stored")
-    return User_end, User_genre, User_main, User_num
-
-
-global User_genre
 
 
 # Challenge function
@@ -67,7 +15,7 @@ def challenge():
         print("okay")
 
 
-# Single question function
+# Single question function - the user is able to choose a specific function to run from the list. The answer is returned to single_ans1
 def single_question():
     global question_choice
     choice_list = ["Type of genre (type genre)", "Type of main character (type main_cha)",
@@ -77,7 +25,7 @@ def single_question():
     choice = [genre_choice, main_cha_choice, num_cha_choice, end_choice]
     print(question_choice)
     if question_choice == "genre":
-        choice[0]()
+        single_ans1 = choice[0]()
     elif question_choice == "main_cha":
         choice[1]()
     elif question_choice == "num_cha":
@@ -87,7 +35,7 @@ def single_question():
 
     else:
         print("Pick choice from list")
-    return genre_choice, main_cha_choice, num_cha_choice, end_choice, question_choice
+    return single_ans1
     pass
 
 
@@ -161,10 +109,15 @@ Choice_for_questions = input("Would you like to run all the questions or just on
 print(Choice_for_questions)
 if Choice_for_questions.lower() in All_answer:
     print("All function")
-    User_end, User_genre, User_main, User_num = all_question()
+    genre = genre_choice()
+    main_character = main_cha_choice()
+    num_char = num_cha_choice()
+    ending = end_choice()
+    print(genre, main_character, num_char, ending)
 elif Choice_for_questions in Single_answer:
     print("Single function")
-    genre_choice(), main_cha_choice, num_cha_choice, end_choice, question_choice, single_question()
+    single_ans = single_question()
+    print(single_ans)
 
 else:
     print("Pick the 2 options")
